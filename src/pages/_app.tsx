710e57b4804dae500e1c3b6a0bd8aa884333b475
baku_nishiya <planet.of.early.summer.rain@gmail.com>
2023-07-24 00:30:39 +0900

    setup nextjs prisma graphql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '~/styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
+
+const NEXT_PUBLIC_GRAPHQL_ENDPOINT = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT
+if (!NEXT_PUBLIC_GRAPHQL_ENDPOINT) {
+  throw new Error('NEXT_PUBLIC_GRAPHQL_ENDPOINT is not defined')
 }
+
+export const client = new ApolloClient({
+  uri: NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+  cache: new InMemoryCache(),
+})
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </SessionProvider>
+  )
+}
+
+export default MyApp
